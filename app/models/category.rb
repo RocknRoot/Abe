14ee07f7f1469bcedb0c3cfca3b33ac4c7c64601
Nekith <nekith@gmail.com>
2011-08-30 14:01:00 +0200

    Remove rails things.

--- app/models/category.rb
+++
@@ -1,6 +0,0 @@
-class Category < ActiveRecord::Base
-  has_many :terms
-  has_many :subcategories, :class_name => "Category",
-    :foreign_key => "parent_id"
-  belongs_to :parent, :class_name => "Category"
-end
